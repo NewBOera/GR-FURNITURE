5d81fbb1ce8b90e3bf4e2880692a7c57fea90cae
Erik Bonifaz <erikbo9@hotmail.com>
2024-07-26 17:53:33 -0600

    first commit

---
+++ scripts/cards-logic.js
@@ -0,0 +1,5 @@
+const $form = document.getElementById("send-form"),
+   firstOffer = document.getElementById("1x"),
+   secondOffer = document.getElementById("2x"),
+   thirdOffer = document.getElementById("3x");
+w;
